337147b50782bd2b1db70ce2de3b401149b9c3b2
xuxiaoya <xuxiaoya@kurogames.com>
2026-05-25 17:53:27 +0800

    feat: 随机点位分布

---
+++ part2/data_view/random_walks/random_walk.py
@@ -0,0 +1,33 @@
+from random import choice
+
+
+class RandomWalk:
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        # All walks start at (0, 0).
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        while len(self.x_values) < self.num_points:
+
+            # 决定走的方向和距离
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            # 不能原地不动
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # 计算新位置
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+
+            self.x_values.append(x)
+            self.y_values.append(y)
